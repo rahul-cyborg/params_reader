d4ce1f4941c14ce20dd39cf111b60a56ea3ab7ec
Rahul Patel <rahul.patel@cybrilla.com>
2016-07-19 18:41:02 +0530

    fix bud

--- lib/params_reader/railtie.rb
+++ lib/params_reader/railtie.rb
@@ -2,5 +2,6 @@ module ParamsReader
   class Railtie < Rails::Railtie
     initializer "params_reader.configure_rails_initialization" do
       Rails.application.middleware.use ParamsReader
+    end
   end
 end
