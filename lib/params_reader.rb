2a6e81875c034cc8f221d914a50ab47d07739cf6
Rahul Patel <rahul.patel@cybrilla.com>
2016-07-19 17:44:37 +0530

    first commit

--- lib/params_reader.rb
+++ lib/params_reader.rb
@@ -1,5 +1,31 @@
 require "params_reader/version"
+require "params_reader/railtie"
 
 module ParamsReader
-  # Your code goes here...
+  class ParamsReader 
+    def initialize(app)
+      @app = app
+    end 
+   
+    def call(env)
+       request = Rack::Request.new(env)
+       if request.params['utm_source'] && request.cookies['utm_source'].nil? && request.path_info == '/'
+           status, headers, body = @app.call(env)
+           response = Rack::Response.new body, status, headers
+           set_parmas_cookie(response, request.params)
+           response.finish 
+      else
+          @app.call(env)
+      end
+    end 
+
+    def set_parmas_cookie(response, params)
+      response.set_cookie("utm_source", {:value => params['utm_source'], :expires => Time.now+360*24*60*60})
+      response.set_cookie("utm_medium", {:value => params['utm_medium'], :expires => Time.now+360*24*60*60})
+      response.set_cookie("utm_term", {:value => params['utm_term'], :expires => Time.now+360*24*60*60})
+      response.set_cookie("utm_content", {:value => params['utm_content'], :expires => Time.now+360*24*60*60})
+      response.set_cookie("utm_campaign", {:value => params['utm_campaign'], :expires => Time.now+360*24*60*60})
+      response.set_cookie("channel", {:value => params['channel'], :expires => Time.now+360*24*60*60})
+    end
+  end
 end
---
+++ lib/params_reader/railtie.rb
@@ -0,0 +1,7 @@
+module ParamsReader
+  class Railtie < Rails::Railtie
+    initializer "params_reader.configure_rails_initialization" do
+      Rails.application.middleware.use ParamsReader::Middleware
+    end
+  end
+end
