45d56733954f30c73fc120fb03c1765ed8ff2615
Rahul Patel <rahul.patel@cybrilla.com>
2016-07-19 18:01:31 +0530

    sadfd

--- lib/params_reader/railtie.rb
+++ lib/params_reader/railtie.rb
@@ -1,6 +1,6 @@
 class Railtie < Rails::Railtie
     initializer "params_reader.configure_rails_initialization" do
-      Rails.application.middleware.use Reader::Middleware
+      Rails.application.middleware.use ParamsReader::Middleware
     end
   end
 
